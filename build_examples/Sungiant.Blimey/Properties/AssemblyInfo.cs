b1e55161a15440800bcbba3768b7ca209a59e14e
Ashleigh Pook <ashleighpook@gmail.com>
2013-07-20 00:07:22 +0100

    Added example build configurations.

---
+++ build_examples/Sungiant.Blimey/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sungiant.Blimey")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+
+//[assembly: InternalsVisibleTo("Sungiant.Blimey.Xna4Runtime_Xna4.x86")]
+//[assembly: InternalsVisibleTo("Sungiant.Blimey.Xna4Runtime_Xna4.Xbox")]
+//[assembly: InternalsVisibleTo("Sungiant.Blimey.Xna4Runtime_Xna4.Wp7")]
+//[assembly: InternalsVisibleTo("Sungiant.Blimey.PsmRuntime_psm")]
+//[assembly: InternalsVisibleTo("Sungiant.Blimey.Xios_xios.device")]
+//[assembly: InternalsVisibleTo("Sungiant.Blimey.Xios_xios.simulator")]
+
+[assembly: CLSCompliant(true)]
