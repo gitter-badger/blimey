09b518f3976f95986a747c57f9e050003f736f55
Ashleigh Pook <ashleighpook@gmail.com>
2013-09-14 23:10:20 +0100

    Updated the Assembly Name in the example build files so that it no longer postfixes with build variant.

--- build_examples/Sungiant.Blimey/Properties/AssemblyInfo.cs
+++ build_examples/Sungiant.Blimey/Properties/AssemblyInfo.cs
@@ -9,13 +9,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-
-
-//[assembly: InternalsVisibleTo("Sungiant.Blimey.Xna4Runtime_Xna4.x86")]
-//[assembly: InternalsVisibleTo("Sungiant.Blimey.Xna4Runtime_Xna4.Xbox")]
-//[assembly: InternalsVisibleTo("Sungiant.Blimey.Xna4Runtime_Xna4.Wp7")]
-//[assembly: InternalsVisibleTo("Sungiant.Blimey.PsmRuntime_psm")]
-//[assembly: InternalsVisibleTo("Sungiant.Blimey.Xios_xios.device")]
-//[assembly: InternalsVisibleTo("Sungiant.Blimey.Xios_xios.simulator")]
-
 [assembly: CLSCompliant(true)]
