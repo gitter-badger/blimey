2ea1b7db5f70f8542db977eac0e77484521fc81c
Ashleigh Pook <ashleighpook@gmail.com>
2013-09-26 23:28:43 +0100

    Automatically convert tabs to spaces.

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,12 +2,22 @@
 
 startPath=`pwd`
 
-# Generate Foundation.cs
 cd ../generate/Foundation
+
+# tabs to spaces
+find ./ ! -type d ! -name _tmp_ -exec sh -c 'expand -t 4 {} > _tmp_ && mv _tmp_ {}' \;
+
+# Generate Foundation.cs
 mono-t4 Foundation.tt -o ../../source/Foundation.cs
+
 cd $startPath
 
-# Generate Foundation.Tests.cs
 cd ../generate/Foundation.Tests
+
+# tabs to spaces
+find ./ ! -type d ! -name _tmp_ -exec sh -c 'expand -t 4 {} > _tmp_ && mv _tmp_ {}' \;
+
+# Generate Foundation.Tests.cs
 mono-t4 Foundation.Tests.tt -o ../../source/Foundation.Tests.cs
+
 cd $startPath
