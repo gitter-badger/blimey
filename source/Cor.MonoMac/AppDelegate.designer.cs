67fc107c0a98b9f995e48d98a694d7f72608e4af
Ashleigh Pook <ashleighpook@gmail.com>
2013-10-01 22:47:30 +0100

    Adding the default empty MonoMac project.

---
+++ source/Cor.MonoMac/AppDelegate.designer.cs
@@ -0,0 +1,10 @@
+
+namespace Cor.MonoMac
+{
+	// Should subclass MonoMac.AppKit.NSResponder
+	[global::MonoMac.Foundation.Register("AppDelegate")]
+	public partial class AppDelegate
+	{
+	}
+}
+
---
+++ source/Cor.MonoMac/MainWindow.designer.cs
@@ -0,0 +1,15 @@
+
+namespace Cor.MonoMac
+{
+	// Should subclass MonoMac.AppKit.NSWindow
+	[global::MonoMac.Foundation.Register("MainWindow")]
+	public partial class MainWindow
+	{
+	}
+	// Should subclass MonoMac.AppKit.NSWindowController
+	[global::MonoMac.Foundation.Register("MainWindowController")]
+	public partial class MainWindowController
+	{
+	}
+}
+
