f7d8acea19f8fe6b8e48f9cb75faba5a1a72b7de
Ashleigh Pook <ashleighpook@gmail.com>
2013-10-01 23:41:26 +0100

    Fixed namespace.

--- source/Cor.StubPlatform.cs
+++ source/Cor.StubPlatform.cs
@@ -42,7 +42,7 @@ using Sungiant.Abacus.Packed;
 using Sungiant.Abacus.SinglePrecision;
 using Sungiant.Abacus.Int32Precision;
 
-namespace Sungiant.Cor.BaseRuntime
+namespace Sungiant.Cor.StubPlatform
 {
     public class StubEngine
         : ICor
