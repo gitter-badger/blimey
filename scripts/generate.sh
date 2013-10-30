846e421b3e9142af5585d662c84be3d31acdc608
Ashleigh Pook <ashleighpook@gmail.com>
2013-10-30 14:22:44 +0000

    Naming convention updates.

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,12 +2,12 @@
 
 startPath=`pwd`
 
-cd ../generate/Cor.MonoMac
+cd ../generate/Cor.Platform.MonoMac
 
 # tabs to spaces
 find ./ ! -type d ! -name _tmp_ -exec sh -c 'expand -t 4 {} > _tmp_ && mv _tmp_ {}' \;
 
-# Generate Cor.MonoMac.cs
-mono-t4 Cor.MonoMac.tt -o ../../source/Cor.MonoMac.cs
+# Generate Cor.Platform.MonoMac.cs
+mono-t4 Cor.Platform.MonoMac.tt -o ../../source/Cor.Platform.MonoMac.cs
 
 cd $startPath
