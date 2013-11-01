259b0f607eca0a7ae905916ac93ee7ad80ef6d1a
Ashleigh Pook <ashleighpook@gmail.com>
2013-11-01 22:35:54 +0000

    Naming scheme updates.

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -7,7 +7,7 @@ cd ../generate/Cor.Platform.Xios
 # tabs to spaces
 find ./ ! -type d ! -name _tmp_ -exec sh -c 'expand -t 4 {} > _tmp_ && mv _tmp_ {}' \;
 
-# Generate Cor.Platform.Xios.cs
-mono-t4 Cor.Platform.Xios.tt -o ../../source/Cor.Platform.Xios.cs
+# Generate Cor.Platform.Managed.Xios.cs
+mono-t4 Cor.Platform.Managed.Xios.tt -o ../../source/Cor.Platform.Managed.Xios.cs
 
 cd $startPath
