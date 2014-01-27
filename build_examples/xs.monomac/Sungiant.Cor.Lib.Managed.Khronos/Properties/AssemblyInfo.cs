1ec11355df9f903766914865625360167599cb77
Ashleigh Pook <ashleighpook@gmail.com>
2014-01-27 17:20:37 +0000

    Rearranging project structure.

---
+++ build_examples/xs.monomac/Sungiant.Cor.Lib.Managed.Khronos/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System;
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sungiant.Cor.Lib.Managed.Khronos")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("Sungiant.Cor.Platform.Managed.Xios")]
+[assembly: InternalsVisibleTo("Sungiant.Cor.Platform.Managed.Xna4")]
+[assembly: InternalsVisibleTo("Sungiant.Cor.Platform.Managed.Psm")]
+[assembly: InternalsVisibleTo("Sungiant.Cor.Platform.Managed.MonoMac")]
