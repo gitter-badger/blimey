51895ecdfa30ab9dad1e98cd7dfbf61600fb7a4e
Ashleigh Pook <ashleighpook@gmail.com>
2014-05-17 14:04:21 +0100

    Travis -_-

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,8 +2,6 @@
 
 startPath=`pwd`
 
-mono ../libs/NUnit-2.6.3/bin/nunit-console.exe ../build/Oats.Tests/bin/Debug/Oats.Tests.dll && exit 1
+mono ../libs/NUnit-2.6.3/bin/nunit-console.exe ../build/Oats.Tests/bin/Debug/Oats.Tests.dll
 
 cd $startPath
-
-exit 0
