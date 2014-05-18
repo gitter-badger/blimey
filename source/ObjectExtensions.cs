ef9858cfbc42247502070db9f51229b28757e3ce
Ashleigh Pook <ashleighpook@gmail.com>
2014-05-18 22:05:47 +0100

    Added a few new helper methods.

--- source/ObjectExtensions.cs
+++ source/ObjectExtensions.cs
@@ -6,10 +6,52 @@ using System.Security.Cryptography;
 
 namespace Oats
 {
-
 	public static class ObjectExtensions
 	{
-		public static Byte[] ToBinary <T> (this Object obj)
+		static MethodInfo toBinaryImplementation = null;
+		static MethodInfo fromBinaryImplementation = null;
+		static MethodInfo toChecksumImplementation = null;
+
+		static ObjectExtensions ()
+		{
+			CacheMethodInfos ();
+		}
+
+		static void CacheMethodInfos ()
+		{
+			toBinaryImplementation = typeof(ObjectExtensions).GetMethod (
+				"ToBinaryImplementation", 
+				BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Default,
+				null,
+				new Type[] { typeof (Object) },
+				null);
+
+			fromBinaryImplementation = typeof(ObjectExtensions).GetMethod (
+				"FromBinaryImplementation", 
+				BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Default,
+				null,
+				new Type[] { typeof (Byte[]) },
+				null);
+
+			toChecksumImplementation = typeof(ObjectExtensions).GetMethod (
+				"ToChecksumImplementation", 
+				BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Default,
+				null,
+				new Type[] { typeof (Object) },
+				null);
+
+			if (toBinaryImplementation == null)
+				throw new Exception ();
+
+			if (fromBinaryImplementation == null)
+				throw new Exception ();
+
+			if (toChecksumImplementation == null)
+				throw new Exception ();
+
+		}
+
+		static Byte[] ToBinaryImplementation <T> (Object obj)
 		{
 			T o = (T) obj;
 
@@ -25,7 +67,7 @@ namespace Oats
 			}
 		}
 
-		public static T FromBinary <T> (this Byte[] bytes)
+		static T FromBinaryImplementation <T> (Byte[] bytes)
 		{
 			using (var stream = new MemoryStream (bytes))
 			{
@@ -39,13 +81,88 @@ namespace Oats
 			}
 		}
 
-		public static Byte[] ToChecksum <T> (this Object obj)
+		static Byte[] ToChecksumImplementation <T> (Object obj)
 		{
 			Byte[] binary = obj.ToBinary <T> ();
 			HashAlgorithm hashAlgorithm = new MD5CryptoServiceProvider ();
 			Byte[] hash = hashAlgorithm.ComputeHash (binary);
 
 			return hash;
+		}
+
+		// ----------------------------------------------------------------------- //
+
+		static Byte[] ToBinaryReflectiveImplementation (Type type, Object obj)
+		{
+			MethodInfo genericMethodInfo = toBinaryImplementation.MakeGenericMethod (new Type [] { type });
+
+			Object result = genericMethodInfo.Invoke (null, new Object[] { obj });
+
+			return (Byte[]) result;
+		}
+
+		static Object FromBinaryReflectiveImplementation (Type type, Byte[] bytes)
+		{
+			MethodInfo genericMethodInfo = fromBinaryImplementation.MakeGenericMethod (new Type [] { type });
+
+			Object result = genericMethodInfo.Invoke (null, new Object[] { bytes });
+
+			return (Byte[]) result;
+		}
+
+		static Byte[] ToChecksumReflectiveImplementation (Type type, Object obj)
+		{
+			MethodInfo genericMethodInfo = toChecksumImplementation.MakeGenericMethod (new Type [] { type });
+
+			Object result = genericMethodInfo.Invoke (null, new Object[] { obj });
+
+			return (Byte[]) result;
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// ----------------------------------------------------------------------- //
+
+		public static Byte[] ToBinaryReflective (this Object obj, Type type)
+		{
+			return ToBinaryReflectiveImplementation (type, obj);
+		}
+
+		public static Object FromBinaryReflective (this Byte[] bytes, Type type)
+		{
+			return FromBinaryReflectiveImplementation (type, bytes);
+		}
+
+		public static Byte[] ToChecksumReflective (this Object obj, Type type)
+		{
+			return ToChecksumReflectiveImplementation (type, obj);
+		}
+
+		// ----------------------------------------------------------------------- //
+
+		public static Byte[] ToBinary <T> (this Object obj)
+		{
+			return ToBinaryImplementation <T> (obj);
+		}
+
+		public static T FromBinary <T> (this Byte[] bytes)
+		{
+			return FromBinaryImplementation <T> (bytes);
+		}
+
+		public static Byte[] ToChecksum <T> (this Object obj)
+		{
+			return ToChecksumImplementation <T> (obj);
 		}
 	}
 }
