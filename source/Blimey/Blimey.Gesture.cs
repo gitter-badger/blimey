e7026126993a24cece8358fe476c044e82dd5600
Ash Pook <ashleighpook@gmail.com>
2015-02-12 14:12:06 +0000

    Updated file headers.

--- source/Blimey/Blimey.Gesture.cs
+++ source/Blimey/Blimey.Gesture.cs
@@ -1,34 +1,38 @@
-// ┌────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐ \\
-// │ Blimey - Fast, efficient, high level engine built upon Cor & Abacus                                            │ \\
-// ├────────────────────────────────────────────────────────────────────────────────────────────────────────────────┤ \\
-// │                     Brought to you by:                                                                         │ \\
-// │                              _________                    .__               __                                 │ \\
-// │                             /   _____/__ __  ____    ____ |__|____    _____/  |_                               │ \\
-// │                             \_____  \|  |  \/    \  / ___\|  \__  \  /    \   __\                              │ \\
-// │                             /        \  |  /   |  \/ /_/  >  |/ __ \|   |  \  |                                │ \\
-// │                            /_______  /____/|___|  /\___  /|__(____  /___|  /__|                                │ \\
-// │                                    \/           \//_____/         \/     \/                                    │ \\
-// │                                                                                                                │ \\
-// ├────────────────────────────────────────────────────────────────────────────────────────────────────────────────┤ \\
-// │ Copyright © 2008-2014 A.J.Pook (http://ajpook.github.io)                                                       │ \\
-// ├────────────────────────────────────────────────────────────────────────────────────────────────────────────────┤ \\
-// │ Authors: A.J.Pook                                                                                              │ \\
-// ├────────────────────────────────────────────────────────────────────────────────────────────────────────────────┤ \\
-// │ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated   │ \\
-// │ documentation files (the "Software"), to deal in the Software without restriction, including without           │ \\
-// │ limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sellcopies of the   │ \\
-// │ Software, and to permit persons to whom the Software is furnished to do so, subject to the following           │ \\
-// │ conditions:                                                                                                    │ \\
-// │                                                                                                                │ \\
-// │ The above copyright notice and this permission notice shall be included in all copies or substantial portions  │ \\
-// │ of the Software.                                                                                               │ \\
-// │                                                                                                                │ \\
-// │ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED  │ \\
-// │ TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL │ \\
-// │ THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF  │ \\
-// │ CONTRACT, TORT OR OTHERWISE, ARISING FROM,OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER        │ \\
-// │ DEALINGS IN THE SOFTWARE.                                                                                      │ \\
-// └────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘ \\
+// ┌────────────────────────────────────────────────────────────────────────┐ \\
+// │ __________.__  .__                                                     │ \\
+// │ \______   \  | |__| _____   ____ ___.__.                               │ \\
+// │  |    |  _/  | |  |/     \_/ __ <   |  |                               │ \\
+// │  |    |   \  |_|  |  Y Y  \  ___/\___  |                               │ \\
+// │  |______  /____/__|__|_|  /\___  > ____|                               │ \\
+// │         \/              \/     \/\/                                    │ \\
+// │                                                                        │ \\
+// │ Cor, Blimey!                                                           │ \\
+// │                                                                        │ \\
+// ├────────────────────────────────────────────────────────────────────────┤ \\
+// │ Copyright © 2012 - 2015 ~ Blimey3D (http://www.blimey3d.com)           │ \\
+// ├────────────────────────────────────────────────────────────────────────┤ \\
+// │ Authors:                                                               │ \\
+// │ ~ Ash Pook (http://www.ajpook.com)                                     │ \\
+// ├────────────────────────────────────────────────────────────────────────┤ \\
+// │ Permission is hereby granted, free of charge, to any person obtaining  │ \\
+// │ a copy of this software and associated documentation files (the        │ \\
+// │ "Software"), to deal in the Software without restriction, including    │ \\
+// │ without limitation the rights to use, copy, modify, merge, publish,    │ \\
+// │ distribute, sublicense, and/or sellcopies of the Software, and to      │ \\
+// │ permit persons to whom the Software is furnished to do so, subject to  │ \\
+// │ the following conditions:                                              │ \\
+// │                                                                        │ \\
+// │ The above copyright notice and this permission notice shall be         │ \\
+// │ included in all copies or substantial portions of the Software.        │ \\
+// │                                                                        │ \\
+// │ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        │ \\
+// │ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     │ \\
+// │ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. │ \\
+// │ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   │ \\
+// │ CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   │ \\
+// │ TORT OR OTHERWISE, ARISING FROM,OUT OF OR IN CONNECTION WITH THE       │ \\
+// │ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 │ \\
+// └────────────────────────────────────────────────────────────────────────┘ \\
 
 namespace Blimey
 {
