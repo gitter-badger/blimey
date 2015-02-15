ce0c07860bce29a98e7e2296e3ed9cf70d4c0684
Ashleigh Pook <ashleighpook@gmail.com>
2015-02-15 10:44:08 +0000

    Code import.

---
+++ build/playstation_mobile_studio/Foundation/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("Foundation")]
+[assembly: AssemblyDescription("Foundation, built with PlayStation Studio, targetting Mono for PlayStation Mobile.")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
+
+
---
+++ build/xamarin_studio.mono40/Foundation/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Foundation")]
+[assembly: AssemblyDescription("Foundation, built with Xamarin Studio, targetting Mono 40.")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(false)]
+
