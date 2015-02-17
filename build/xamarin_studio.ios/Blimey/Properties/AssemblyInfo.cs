61bba8f9b3aec32c02d21f301340eb335fec49f5
Ashleigh Pook <ashleighpook@gmail.com>
2015-02-17 08:45:23 +0000

    Updated build files.

---
+++ build/xamarin_studio.ios/Blimey/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Blimey")]
+[assembly: AssemblyDescription("Blimey, built with Xamarin Studio, targetting Xamarin iOS.")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(false)]
+
