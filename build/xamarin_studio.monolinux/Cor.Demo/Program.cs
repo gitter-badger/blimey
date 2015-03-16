8d0f4301fa278216811c9c9cdc45aba939cdde27
Ryan Sullivan <ryan.philip.sullivan@gmail.com>
2015-03-16 18:02:22 +0000

    Add demo using the linux platform;
    - tested on arch linux

--- build/xamarin_studio.monolinux/Cor.Demo/Program.cs
+++ build/xamarin_studio.monolinux/Cor.Demo/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using Cor;
+using Platform.Linux;
+using Platform;
 
 namespace Cor.Demo
 {
 	public class Program
 	{
-		public void Main( string[] args )
+		public static void Main( string[] args )
 		{
-			Console.WriteLine( "Hello World" );
+			var appSettings = new AppSettings ("Cor") {
+				FullScreen = true,
+				MouseGeneratesTouches = true
+			};
+
+			var entryPoint = new BasicApp();
+
+			MonoLinuxPlatform platform = new MonoLinuxPlatform ();
+
+			#if LOGGED_PROXY
+			memory = new FileStream (Path.Combine (Environment.GetEnvironmentVariable ("HOME"), "log.txt"), FileMode.Create);
+			writer = new StreamWriter (memory);
+			using( var engine = new Engine(new LoggedProxyPlatform (platform, writer), appSettings, entryPoint))
+			{
+				platform.run();
+			}
+			#else
+			using( var engine = new Engine(
+				platform,
+				appSettings,
+				entryPoint) )
+			{
+				platform.Run();
+			}
+			#endif
 		}
 	}
 }
